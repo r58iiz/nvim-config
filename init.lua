af38084ffdf0bf584e179b853915203059ea1a85
EvadingCrossbowHashbrown <39996926+r58iiz@users.noreply.github.com>
2025-12-26 22:37:24 +0530

    refactor(init): restructure config file loading
    - update package.path to use vim.fs.joinpath
    - change config_files to use tables for entries
    - modify loading logic to handle entry.startup

--- init.lua
+++ init.lua
@@ -1,22 +1,39 @@
 local HOME = vim.env.HOME or vim.env.USERPROFILE
-package.path = HOME .. "/.config/nvim/lua/?.lua;" .. package.path
+package.path = vim.fs.joinpath(HOME, ".config/nvim/lua/?.lua") .. ";" .. package.path
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local config_files = {
-    "options",
-    "keymaps",
-    "plugin_loader",
-    "autocmds",
+    { name = "options" },
+    { name = "autocmds" },
+    {
+        name = "plugin_loader",
+        startup = function(mod)
+            mod["setup"]({
+                config_path = vim.fs.joinpath(vim.env.HOME, ".config", "nvim", "plugin_state.json"),
+            })
+            mod["start"]()
+        end,
+    },
+    { name = "keymaps" },
 }
 
-for _, file in pairs(config_files) do
-    local success, res = pcall(require, file)
-    if not success then
-        vim.notify("[!] Failed to load " .. file .. " config. Error:\n" .. res, vim.log.levels.ERROR)
+for _, entry in ipairs(config_files) do
+    local ok, mod = pcall(require, entry.name)
+
+    if not ok then
+        vim.notify("[!] Failed to load " .. entry.name .. ". Error:\n" .. mod, vim.log.levels.ERROR)
+        goto continue
     end
-    if type(res) ~= "boolean" and res["start"] then
-        res["start"]()
+
+    if type(entry.startup) == "function" then
+        entry.startup(mod)
+    elseif type(mod) == "table" and type(mod.setup) == "function" then
+        mod.setup(entry.opts)
+    elseif type(mod) == "table" and type(mod.start) == "function" then
+        mod.start()
     end
+
+    ::continue::
 end
